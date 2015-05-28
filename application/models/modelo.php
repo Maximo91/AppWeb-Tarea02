f4d6ba187e1f8e6daa906df4335943a7103e9511
Maximo Martinez <m.martinez.villarroel@gmail.com>
2015-05-28 16:48:02 -0400

    ITERACION - 03
    Mejora consulta SQL - Agregar JOIN
    
    En esta iteración modificaremos las consultas realizadas a la base de
    datos, para así analizar sus diferentes tiempos de respuestas.

--- application/models/modelo.php
+++ application/models/modelo.php
@@ -10,7 +10,11 @@ class modelo extends CI_Model{
    		return $consulta->result();
     }
     public function consulta_media(){
-    	$consulta = $this->db->query('SELECT de.emp_no,d.dept_no FROM departments as d JOIN dept_emp as de WHERE d.dept_no = "d008" and de.dept_no = "d008" and de.emp_no < 10999;
+    	$consulta = $this->db->query('SELECT de.emp_no,d.dept_no FROM 
+              departments as d JOIN dept_emp as de WHERE 
+             d.dept_no = "d008" and 
+             de.dept_no = "d008" and 
+             de.emp_no < 10999;
 ');
    		return $consulta->result();
     }
@@ -19,15 +23,15 @@ class modelo extends CI_Model{
 
     public function consulta_compleja(){
     	$consulta = $this->db->query('SELECT title,birth_date,salary, first_name,last_name,hire_date FROM 
-                departments as d , dept_emp as de , employees as e , titles as t , salaries as s WHERE 
-                d.dept_no = de.dept_no and
-                de.emp_no = e.emp_no and 
-                e.emp_no = s.emp_no and 
-                e.emp_no = t.emp_no and 
-                s.emp_no = t.emp_no and 
-                s.emp_no = de.emp_no and
-                t.emp_no = de.emp_no and
-                d.dept_no = "d008" ');
+             departments as d JOIN dept_emp as de JOIN employees as e JOIN titles as t  JOIN salaries as s WHERE 
+             d.dept_no = de.dept_no and
+             de.emp_no = e.emp_no and 
+             e.emp_no = s.emp_no and 
+             e.emp_no = t.emp_no and 
+             s.emp_no = t.emp_no and 
+             s.emp_no = de.emp_no and
+             t.emp_no = de.emp_no and
+             d.dept_no = "d008";');
    		return $consulta->result();
     }
 }
